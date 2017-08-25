dd5be120370b79d3e975e86505c79ccfaed92c42
isaapons <isaapons@student.arteveldehs.be>
2017-08-26 00:41:18 +0200

    test 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function() {
   $('#fullpage').fullpage({
+    sectionsColor: ['#ffd68b','#7FCEFF'],
     verticalCentered: false,
+    anchors:['one', 'two', 'thirdPage'],
   });
 });
