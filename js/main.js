60d76e871c754690f9e88eef9f4f11ff3608dfde
isaapons <isaapons@student.arteveldehs.be>
2017-08-29 01:18:46 +0200

    fancy navigation boy

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 window.onload = function() {
+// VARIABLES
+var burger = document.querySelector(".burger");
+var secretNav = document.querySelector(".secret-nav");
+var header = document.querySelector("header");
+var main = document.querySelector("main");
 
-  // FULLPAGEJS
-  /*  $(document).ready(function() {
-  $('#fullpage').fullpage({
-  sectionsColor: ['#ffd68b','#7FCEFF'],
-  verticalCentered: false,
-  anchors:['one', 'two'],
-  navigation: true,
-  responsiveWidth: 960,
-  scrollBar: false,
-
-});
-}); */
+// RESPONSIVE NAVIGATION
+burger.addEventListener("click", function() {
+    secretNav.classList.toggle("secret-nav-visible");;
+    burger.classList.toggle("fa-bars");
+    burger.classList.toggle("fa-times");
+    header.classList.toggle("header-move");
+    main.classList.toggle("header-move");
+  });
 };
 
 //#7FCEFF
