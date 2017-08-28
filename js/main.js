944c3da90b5c2b4285c2e5054208455256988960
isaapons <isaapons@student.arteveldehs.be>
2017-08-28 23:45:33 +0200

    made a header and ditched fullpagejs

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 window.onload = function() {
 
   // FULLPAGEJS
-  $(document).ready(function() {
-    $('#fullpage').fullpage({
-      sectionsColor: ['#ffd68b','#ffd68b'],
-      verticalCentered: false,
-      anchors:['one', 'two'],
-      navigation: true,
-      responsiveWidth: 960,
-      scrollBar: false,
+  /*  $(document).ready(function() {
+  $('#fullpage').fullpage({
+  sectionsColor: ['#ffd68b','#7FCEFF'],
+  verticalCentered: false,
+  anchors:['one', 'two'],
+  navigation: true,
+  responsiveWidth: 960,
+  scrollBar: false,
 
-    });
-  });
+});
+}); */
 };
 
 //#7FCEFF
