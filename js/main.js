1220e12cb754d9c3694f0d83d676d46eefb92cba
isaapons <isaapons@student.arteveldehs.be>
2017-08-29 15:16:46 +0200

    gradients are cool?

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 window.onload = function() {
-// VARIABLES
-var burger = document.querySelector(".burger");
-var secretNav = document.querySelector(".secret-nav");
-var header = document.querySelector("header");
-var main = document.querySelector("main");
+  // VARIABLES
+  var burger = document.querySelector(".burger");
+  var secretNav = document.querySelector(".secret-nav");
+  var header = document.querySelector("header");
+  var main = document.querySelector("main");
+// SCROLL TO SECTIONS
 
-// RESPONSIVE NAVIGATION
-burger.addEventListener("click", function() {
-    secretNav.classList.toggle("secret-nav-visible");;
+
+$('a[href*=\\#]').on('click', function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+});
+  // RESPONSIVE NAVIGATION
+  burger.addEventListener("click", function() {
+    secretNav.classList.toggle("secret-nav-visible");
     burger.classList.toggle("fa-bars");
     burger.classList.toggle("fa-times");
     header.classList.toggle("header-move");
     main.classList.toggle("header-move");
+
   });
 };
 
