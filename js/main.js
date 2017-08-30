ec9767732740a6b0e5708ea738c77c38161f3ca9
isaapons <isaapons@student.arteveldehs.be>
2017-08-30 17:00:42 +0200

    this is getting solid

--- js/main.js
+++ js/main.js
@@ -19,28 +19,28 @@ window.onload = function() {
 
   //CHANGE SOUNDCLOUD LINKS ATTEMPT REALLY DUMB WAY TO DO THIS
   song1.addEventListener("click", function() {
-    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/304123823&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
+    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/304123823&amp;color=dd5733&amp;auto_play=true&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
   });
   song2.addEventListener("click", function() {
-    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/286897038&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
+    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/286897038&amp;color=dd5733&amp;auto_play=true&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
   });
   song3.addEventListener("click", function() {
-    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/264093191&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
+    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/264093191&amp;color=dd5733&amp;auto_play=true&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
   });
   song4.addEventListener("click", function() {
-    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/239648075&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
+    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/239648075&amp;color=dd5733&amp;auto_play=true&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
   });
   song5.addEventListener("click", function() {
-    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/279908460&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
+    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/279908460&amp;color=dd5733&amp;auto_play=true&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
   });
   song6.addEventListener("click", function() {
-    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/317416811&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
+    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/317416811&amp;color=dd5733&amp;auto_play=true&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
   });
   song7.addEventListener("click", function() {
-    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/339584709&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
+    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/339584709&amp;color=dd5733&amp;auto_play=true&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
   });
   song8.addEventListener("click", function() {
-    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/321580280&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
+    $(".sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/321580280&amp;color=dd5733&amp;auto_play=true&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
   });
 
   // SCROLL TO SECTIONS
@@ -59,10 +59,12 @@ window.onload = function() {
   // JUSTIFIED GALLERY
   $('.music-gallery').justifiedGallery({
     rowHeight : 200,
-    lastRow : 'nojustify',
+    lastRow : 'hide',
     margins : 0,
     cssAnimation : true,
-    waitThumbnailsLoad : false
+    waitThumbnailsLoad : false,
+    maxRowHeight:	200,
+    randomize: true
   });
 };
 
